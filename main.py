5ef94a8d04d013fb6a445a7035b08155628a2d88
Aze <63929044+AzeBah@users.noreply.github.com>
2020-05-24 18:36:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ response = requests.get(
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"}
 )
 
+position = response.text.index("contentUrl")
+position = response.text.index(":", position)
+position = response.text.index('"', position)
+position += 1
+secondPos = response.text.index('"', position)
+contentUrl = response.text[position:secondPos]
+
+
 print(response.text)
+print(contentUrl)
