dea141cfb2931e57c0db42e831277391932d8fc7
Aze <63929044+AzeBah@users.noreply.github.com>
2020-05-24 18:58:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 
 
+
+tiktokVideoLink = input("enter video link to downlaod : ")
+
 response = requests.get(
-    "https://vm.tiktok.com/wg5bUp/",
+    tiktokVideoLink,
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"}
 )
 
@@ -14,5 +17,12 @@ secondPos = response.text.index('"', position)
 contentUrl = response.text[position:secondPos]
 
 
-print(response.text)
-print(contentUrl)
+
+
+response = requests.get(
+    contentUrl,
+)
+
+f = open("tiktokVideo.mp4" , "wb")
+f.write(response.content)
+f.close()
